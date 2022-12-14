8a66d3099f66cecdb9442ac0654cb4106ffd2734
Dima <dmitryberesten@gmail.com>
2022-12-14 16:21:59 +0200

    Додає плавне зникнення якоря

--- src/js/custom-elements.js
+++ src/js/custom-elements.js
@@ -48,31 +48,18 @@ function myFunction() {
 // -------------CUSTOM-HORIZONTAL-INDICATOR-[END]-------------
 
 // -------------CUSTOM-ANCHOR-[START]-------------
-$(document).ready(function () {
-    // Add smooth scrolling to all links
-    $('a').on('click', function (event) {
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== '') {
-            // Prevent default anchor click behavior
-            event.preventDefault();
 
-            // Store hash
-            var hash = this.hash;
+//Make sure the user has scrolled at least double the height of the browser
+var toggleHeight = $(window).outerHeight() * 1.2;
 
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate(
-                {
-                    scrollTop: $(hash).offset().top,
-                },
-                //duration
-                0,
-                function () {
-                    // Add hash (#) to URL when done scrolling (default click behavior)
-                    window.location.hash = hash;
-                }
-            );
-        }
-    });
+$(window).scroll(function () {
+    if ($(document).scrollTop() > toggleHeight) {
+        //Adds active class to make button visible
+        $("#up-button").addClass("active");
+
+    } else {
+        //Removes active class to make button visible
+        $("#up-button").removeClass("active");
+    }
 });
 // -------------CUSTOM-ANCHOR-[END]-------------
